2836ed7d9e777bfc2f1522cb52b7839ee01cd3af
Taylor Holliday <wtholliday@gmail.com>
2024-01-29 11:19:30 -0800

    Update build_command.sh

--- build_command.sh
+++ build_command.sh
@@ -1,4 +1,4 @@
 
 # ./build.sh --config Release --use_xcode --ios --apple_sysroot iphoneos --osx_arch arm64 --apple_deploy_target 17.2 --skip_tests
 
-python3 tools/ci_build/github/apple/build_apple_framework.py tools/ci_build/github/apple/default_full_apple_framework_build_settings.json --config Release
+python3 tools/ci_build/github/apple/build_apple_framework.py tools/ci_build/github/apple/default_mobile_visionos_framework_build_settings.json --config Release
